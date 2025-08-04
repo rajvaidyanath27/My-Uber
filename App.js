b2d37642ec6894eb94901474c10c6b92a2736e19
Raj <rajbaidyanath76@gmail.com>
2025-08-04 13:37:41 +0530

    kjn

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import {Provider} from 'react-redux';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen'
+import MapScreen from './screens/MapScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from "@react-navigation/native";
+
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -19,6 +21,12 @@ export default function App() {
             <Stack.Screen
                 name='HomeScreen'
                 component={HomeScreen}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name='MapScreen'
+                component={MapScreen}
+                options={{headerShown: false}}
             />
           </Stack.Navigator>
         </SafeAreaProvider>
---
+++ screens/MapScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const MapScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-500 text-xl">Hi</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
